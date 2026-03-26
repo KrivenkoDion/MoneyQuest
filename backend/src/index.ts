6a33da7ecc39e64a1f1d8877d93f5ee856381eab
KrivenkoDion <dion.krivenko@student.howest.be>
2026-03-26 02:30:16 +0100

    Render added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -90,6 +90,8 @@ app.get("/profile", authMiddleware, (req: any, res) => {
 });
 
 
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
